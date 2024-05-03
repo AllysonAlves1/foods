d8953eb985f960b565db9a394cbb2b175d56cedb
Allyson <allysonalves.01@gmail.com>
2024-05-03 10:55:22 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
     <div className="m-2  p-2">
       <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
     </div>
   );
 }
